767f587d82cba5f4aed5e8ab2bfee6c2382fafc6
jsturtz <jabestur@gmail.com>
2019-05-06 13:10:24 -0700

    Finished parallel programming 3 assignment. Finished writing sturtzj.buildrooms.c for OS project 2

--- os/project2/sturtzj.buildrooms.c
+++ os/project2/sturtzj.buildrooms.c
@@ -6,18 +6,15 @@
 #include <string.h>
 #include <time.h>
 
-#define START_ROOM      0
-#define MID_ROOM        1
-#define END_ROOM        2
-
 struct Room 
 {
   char* name;
   int numConnections;
   struct Room* connections[6];
-  int type;
+  char* type;
 };
 
+/* makes unique directory by concatenating process ID */
 void makeDirectory(char* dir) {
 
   sprintf(dir, "sturtzj.rooms.%d", getpid());
@@ -25,36 +22,62 @@ void makeDirectory(char* dir) {
   mkdir(dir, S_IRUSR | S_IWUSR | S_IXUSR);
 }
 
+/* will make the files that should populate the directory named by dir with values from rooms array*/
+void makeFiles(struct Room rooms[7], char* dir) {
+
+  int r, c;  
+  FILE* file;
+  char path[50];
+  for (r = 0; r < 7; r++) {
+    memset(path, '\0', 50);
+    strcpy(path, dir);                  /* copy directory characters first */
+    strcat(path, "/");                  /* concat the path symbol */
+    strcat(path, rooms[r].name);        /* concat the name of room */
+    strcat(path, "_room");              /* final extra bit */
+    file = fopen(path, "w");
+    fprintf(file, "ROOM NAME: %s\n", rooms[r].name);
+    for (c = 0; c < rooms[r].numConnections; c++) {
+      fprintf(file, "CONNECTION %d: %s\n", c+1, rooms[r].connections[c]->name);
+    }
+    fprintf(file, "ROOM TYPE: %s\n", rooms[r].type);
+    fclose(file);
+  }
+}
+
+/* initializes structs, make sure to call deleteRooms below */
 void initRooms(struct Room rooms[7]) {
   int i, j;
 
   for (i = 0; i < 7; i++) {
     char* name = calloc(9, sizeof(char));
+    char* type = calloc(11, sizeof(char));
     rooms[i].name = name;
+    rooms[i].type = type;
     rooms[i].numConnections = 0;
-    rooms[i].type = -1;
     for (j = 0; j < 6; j++) {
       rooms[i].connections[j] = NULL;
     }
   }
 }
 
+/* necessary to free up space allocated on heap */
 void deleteRooms(struct Room rooms[7]) {
   int i;  
   for (i = 0; i < 7; i++) {
     free(rooms[i].name);
+    free(rooms[i].type);
   }
 }
 
 /* assigns START_ROOM, END_ROOM, and MID_ROOM types to the seven rooms */
 void assignTypes(struct Room rooms[7]) {
-
-  rooms[0].type = START_ROOM;
-  rooms[6].type = END_ROOM;
+  
+  strcpy(rooms[0].type, "START_ROOM");
+  strcpy(rooms[6].type, "END_ROOM");
   
   int i;
-  for (i = 1; i < 7; i++) {
-    rooms[i].type = MID_ROOM;
+  for (i = 1; i < 6; i++) {
+    strcpy(rooms[i].type, "MID_ROOM");
   }
 }
 
@@ -100,19 +123,25 @@ int CanAddConnectionFrom(struct Room* x)
 /* Returns true if a connection from Room x to Room y already exists, false otherwise */
 int ConnectionAlreadyExists(struct Room* x, struct Room* y)
 {
-  /* ... */
+  int i;
+  for (i = 0; i < x->numConnections; i++) {
+    if (x->connections[i] == y) 
+      return 1;
+  }
+  return 0;
 }
 
 /* Connects Rooms x and y together, does not check if this connection is valid */
 void ConnectRoom(struct Room* x, struct Room* y) 
 {
-  /* ... */
+  x->connections[x->numConnections] = y;
+  x->numConnections++;
 }
 
 /* Returns true if Rooms x and y are the same Room, false otherwise */
 int IsSameRoom(struct Room* x, struct Room* y) 
 {
-  /* ... */
+  return x == y;
 }
 
 /* Adds a random, valid outbound connection from a Room to another Room */
@@ -140,157 +169,34 @@ void AddRandomConnection(struct Room rooms[7])
 }
 
 int main() {
-  
+
   srand(time(0));
 
+  /* hardcode ten room names */
+  char* roomNames[10] = {
+    "Socrates", "Camus", "Sartre", "Marx", "Proudhon", 
+    "Bakunin", "Soren", "Hegel", "Cohen", "Chomsky"
+  };
+
+  /* set up rooms */
+  struct Room rooms[7];
+  initRooms(rooms);                     /* initializes with values */
+  assignNames(rooms, roomNames);        /* assigned names randomly from ten roomNames */
+  assignTypes(rooms);                   /* assigns first in rooms as START_ROOM, last as END_ROOM, rest as MID_ROOM */
+  
+  /* fully add connections to meet requirements */
+  while (IsGraphFull(rooms) == 0)
+  {
+    AddRandomConnection(rooms);
+  }
+
+  /* make directory for files */
   char dir[20];
   memset(dir, '\0', 20);
   makeDirectory(dir);
   
-  struct Room rooms[7];
-
-  char* roomNames[10] = {
-    "Socrates", 
-    "Camus", 
-    "Sartre", 
-    "Marx", 
-    "Proudhon", 
-    "Bakunin", 
-    "Soren K", 
-    "Hegel", 
-    "Cohen", 
-    "Chomsky"
-  };
-
-  printf("testing roomnames: %s\n", roomNames[0]);
-  
-  initRooms(rooms);
-  assignNames(rooms, roomNames);
-  assignTypes(rooms);
-
-  int test;
-  test = IsGraphFull(rooms);
-  printf("Testing IsGraphFull, should be 0: %d\n", test);
-  
-  int i;
-  for (i = 0; i < 7; i++) {
-    rooms[i].numConnections = 3;
-  }
-  
-  char* testname = "This is a test";
-  rooms[1].name = testname;
-
-  rooms[0].connections[0] = &rooms[1];
-  rooms[0].connections[1] = &rooms[2];
-  rooms[0].connections[2] = &rooms[3];
-  rooms[1].connections[0] = &rooms[2];
-  rooms[1].connections[1] = &rooms[3];
-  rooms[1].connections[2] = &rooms[4];
-  rooms[2].connections[0] = &rooms[1];
-  rooms[2].connections[1] = &rooms[3];
-  rooms[2].connections[2] = &rooms[4];
-  rooms[3].connections[0] = &rooms[2];
-  rooms[3].connections[1] = &rooms[1];
-  rooms[3].connections[2] = &rooms[4];
-  rooms[4].connections[0] = &rooms[2];
-  rooms[4].connections[1] = &rooms[3];
-  rooms[4].connections[2] = &rooms[5];
-  rooms[5].connections[0] = &rooms[2];
-  rooms[5].connections[1] = &rooms[3];
-  rooms[5].connections[2] = &rooms[4];
-  rooms[6].connections[0] = &rooms[2];
-  rooms[6].connections[1] = &rooms[3];
-  rooms[6].connections[2] = &rooms[4];
-  
-
-  test = IsGraphFull(rooms);
-  printf("Testing IsGraphFull, should be 1: %d\n", test);
-
-  printf("Testing connection O->1\n");
-  printf("Name of 0->1: %s\n", rooms[0].connections[0]->name);
-
-  printf("Testing assignment of names\n");
-  for (i = 0; i < 7; i++)
-    printf("%s\n", rooms[i].name);
-
-  printf("Testing getRandomRoom. Printing random names from func:\n");
-  for (i = 0; i < 7; i++) {
-    struct Room* next = GetRandomRoom(rooms);
-    printf("%s\n", next->name);
-  }
-
-  printf("Testing CanAddConnection...\n");
-  rooms[0].numConnections = 7;
-  printf("Should be 0: %d\n", CanAddConnectionFrom(&rooms[0]));
-
-  rooms[1].numConnections = 3;
-  printf("Should be 1: %d\n", CanAddConnectionFrom(&rooms[1]));
-
-
-  
-
-  /* while (IsGraphFull(rooms) == 0) */
-  /* { */
-  /*   AddRandomConnection(); */
-  /* } */
-
-  /* char** randomNames[7] = malloc(7 * sizeof(char*)); */
-
-  /* char* randomNames[7]; */
-  /* randomNames = GetRandom(roomNames, 7); */
-
-  /* printf("Process ID: %d\n", pid); */
-  /* FILE* file = fopen("newdir/test.txt", "w"); */
-  /* fprintf(file, "This is the new shit\n"); */
-  /* fprintf(file, "This is the second new shit\n"); */
-  /* fprintf(file, "This is the overridden shit\n"); */
-  /* fclose(file); */
-    
-  /* char file[] = "newdir/test.txt"; */
-  /* int file_descriptor = open(file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR); */
+  /* loop through rooms and create one file per room */
+  makeFiles(rooms, dir);
+  deleteRooms(rooms);
   return 0;
 }
-
-/* void testingSomeShit() { */
-
-/*   char file[] = "testFile.txt"; */
-/*   int file_descriptor; */
-
-/*   file_descriptor = open(file, O_WRONLY | O_TRUNC); */
-/*   if (file_descriptor < 0) { */
-/*     fprintf(stderr, "Weird problem bro\n"); */
-/*   } */
-/*   else */ 
-/*     printf("This shit will open up a file for writing and delete its existing contents brah. It\'s bitwise ORing the shit out of those bits brah\n"); */
-
-/*   close(file_descriptor); */
-
-/*   file_descriptor = open(file, O_WRONLY | O_APPEND); */
-/*   if (file_descriptor < 0) { */
-/*     fprintf(stderr, "Weird problem bro\n"); */
-/*   } */
-/*   else */ 
-/*     printf("Opens for writing by appending to file\n") */
-
-/*   close(file_descriptor); */
-
-/*   char noFile[] = "notExistFile.txt"; */
-/*   file_descriptor = open(noFile, O_WRONLY | O_CREAT, 0600); */
-/*   // the O_CREAT tells C to create a file if it doesn't exist, but the file */
-/*   // must also be given the right permissions, which is that number at the */ 
-/*   // end which is some OCTAL shit that figures out your permissions */
-  
-/*   // alternatively: */ 
-/*   file_descriptor = open(noFile, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR); */
-
-/*   // lseek shit */
-/*   newpos = lseek(file_descriptor, 16, SEEK_SET); // moves 16 bytes from start */
-/*   newpos = lseek(file_descriptor, 4, SEEK_CUR); // moves 4 bytes from currentpos */
-/*   newpos = lseek(file_descriptor, 4, SEEK_END); // moves 4 bytes from endFile */
-
-
-/*   // read() shit */
-/*   int buffer[100];      // create buffer to get input from file */
-/*   read(file_descriptor, &buffer, 10);   // reads ten bytes from file into buffer */
-/*   write(file_descriptor, &buffer, 4);   // writes four bytes FROM buffer into f */
-/* } */
--- para/project3/project3.cpp
+++ para/project3/project3.cpp
@@ -1,7 +1,12 @@
-
-#ifndef
-#define NUMT 10
-#endif
+#include <string>
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <omp.h>
+#include <ctype.h>
+using namespace std;
 
 // "state" of the system involves these globals
 int	NowYear;		// 2019 - 2024
@@ -10,6 +15,7 @@ float	NowPrecip;		// inches of rain per month
 float	NowTemp;		// temperature this month
 float	NowHeight;		// grain height in inches
 int	NowNumDeer;		// number of deer in the current population
+int	ZombieBearAttack = 0;   // a boolean for whether zombie bears attack
 
 // Other parameters
 const float GRAIN_GROWS_PER_MONTH =		8.0;    // in inches
@@ -26,69 +32,49 @@ const float RANDOM_TEMP =			10.0;	// plus or minus noise
 const float MIDTEMP =				40.0;
 const float MIDPRECIP =				10.0;
 
-
-// temp and precip are functions of particular month:
-float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
-float temp = AVG_TEMP - AMP_TEMP * cos( ang );
-
-unsigned int seed = 0;
-NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
-float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
-NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
-if ( NowPrecip < 0. ) NowPrecip = 0.;
-
-// starting date and time:
-NowMonth =    0;
-NowYear  = 2019;
-
-// starting state (feel free to change this if you want):
-NowNumDeer = 1;
-NowHeight =  1.;
-
-/* In addition to this, you must add in some other phenomenon that directly or indirectly controls the growth of the grain and/or the graindeer population. Your choice of this is up to you. */ 
-
-omp_set_num_threads( 4 );	// same as # of sections
-initBarrier(4);
-
-void MyAgent() 
-{
-  while (NowYear < 2025) {
-    printf("Agent waiting at #1\n");
-    #pragma omp barrier       
-    printf("Agent resuming at #1\n");
-
-    printf("Agent waiting at #2\n");
-    #pragma omp barrier       
-    printf("Agent resuming at #2\n");
-
-    // write out "Now" state of data
-    // Advance line and re-compute environmental variables
-    
-    printf("Agent waiting at #3\n");
-    #pragma omp barrier       
-    printf("Agent resuming at #3\n");
-  }
-}
-
-float
-SQR( float x )
+// Joe Parallel functions
+float SQR( float x )
 {
   return x*x;
+} 
+
+float Ranf( unsigned int *seedp,  float low, float high )
+{
+  float r = (float) rand_r( seedp );              // 0 - RAND_MAX
+  return(   low  +  r * ( high - low ) / (float)RAND_MAX   );
 }
 
-void GrainDeer() 
+
+int Ranf( unsigned int *seedp, int ilow, int ihigh )
+{
+  float low = (float)ilow;
+  float high = (float)ihigh + 0.9999f;
+
+  return (int)(  Ranf(seedp, low,high) );
+}
+
+// ZombieBears are lazy zombies. They only come out when the weather is nice: 
+// Warm temperatures with little to no rain. But when they do come out, they
+// kill everything in sight. When there is a ZombieBearAttack, half the
+// deer population is wiped out. 
+
+void ZombieBears() 
 {
   while (NowYear < 2025) {
     
-    // compute temporary next value based on current state
-    float tempFactor = exp(-SQR(( NowTemp - MIDTEMP ) / 10.  ));
-    float precipFactor = exp(-SQR(( NowPrecip - MIDPRECIP ) / 10.));
-
+    int attack;
+    if (NowTemp > 60 && NowPrecip < 10)
+      attack = 1; 
+    else
+      attack = 0;
+    
     // done computing barrier
     #pragma omp barrier       
-
-    NowHeight += tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH;
-    NowHeight -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH;
+    
+    if (attack) 
+      ZombieBearAttack = 1;
+    else
+      ZombieBearAttack = 0;
 
     // done assigning barrier 
     #pragma omp barrier       
@@ -98,7 +84,133 @@ void GrainDeer()
   }
 }
 
+void GrainDeer() 
+{
+  while (NowYear < 2025)
+  {
+    // compute temporary next value to add based on current state
+    int tempVal;
+
+    if (ZombieBearAttack) 
+      tempVal = NowNumDeer / 2;
+    else 
+    {
+      if (NowNumDeer > NowHeight)
+        tempVal = NowNumDeer-1;
+      else 
+        tempVal = NowNumDeer+1;
+    }
+
+    // done computing barrier
+    #pragma omp barrier       
+    
+    NowNumDeer = tempVal;
+    // done assigning barrier 
+    #pragma omp barrier       
+
+    // done printing barrier
+    #pragma omp barrier       
+  }
+}
+
+void Grain() 
+{
+  while (NowYear < 2025) {
+    
+    // compute temporary next value based on current state
+    float tempVal = 0;
+    float tempFactor = exp(-SQR(( NowTemp - MIDTEMP ) / 10.));
+    float precipFactor = exp(-SQR(( NowPrecip - MIDPRECIP ) / 10.));
+    tempVal += tempFactor * precipFactor * GRAIN_GROWS_PER_MONTH;
+    tempVal -= (float)NowNumDeer * ONE_DEER_EATS_PER_MONTH;
+
+    // done computing barrier
+    #pragma omp barrier       
+
+    NowHeight += tempVal;
+
+    // prevents NowHeight from being set to negative value
+    if (NowHeight < 0) 
+    {
+      NowHeight = 0;
+    }
+
+    // done assigning barrier 
+    #pragma omp barrier       
+
+    // done printing barrier
+    #pragma omp barrier       
+  }
+}
+
+void Watcher() 
+{
+  while (NowYear < 2025) {
+    
+    // done computing barrier
+    #pragma omp barrier       
+
+    // done assigning barrier 
+    #pragma omp barrier
+    
+    // Adjust month and year
+    if (NowMonth < 11) 
+    {
+      NowMonth += 1;
+    }
+    else
+    {
+      NowMonth = 0; 
+      NowYear += 1;
+    }
+
+    // Use new month to adjust temp and precip
+    float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
+
+    float temp = AVG_TEMP - AMP_TEMP * cos( ang );
+    unsigned int seed = rand();
+    NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
+
+    float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
+    NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
+    if( NowPrecip < 0. )
+      NowPrecip = 0.;
+
+    printf("%d\t%d\t%8.2f\t%8.2f\t%8.2f\t%d\t%d\n", NowYear, NowMonth, NowPrecip, NowTemp, NowHeight, NowNumDeer, ZombieBearAttack);
+    // done printing barrier
+    #pragma omp barrier       
+  }
+}
+
 int main() {
+  
+  srand(time(0));
+
+  // starting date and time:
+  NowMonth =    0;
+  NowYear  = 2019;
+
+  // starting state (feel free to change this if you want):
+  NowNumDeer = 1;
+  NowHeight =  1.;
+
+  // temp and precip are functions of particular month:
+  float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
+
+  float temp = AVG_TEMP - AMP_TEMP * cos( ang );
+  unsigned int seed = 0;
+  NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
+
+  float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
+  NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
+  if( NowPrecip < 0. )
+    NowPrecip = 0.;
+
+  /* In addition to this, you must add in some other phenomenon that directly or indirectly controls the growth of the grain and/or the graindeer population. Your choice of this is up to you. */ 
+
+  printf("Year\tMonth\tPrecip\t\tTemp\t\tHeight\t\tNumDeer\tZombieBears?\n");
+  printf("%d\t%d\t%8.2f\t%8.2f\t%8.2f\t%d\t%d\n", NowYear, NowMonth, NowPrecip, NowTemp, NowHeight, NowNumDeer, ZombieBearAttack);
+  omp_set_num_threads( 4 );	// same as # of sections
 
 #pragma omp parallel sections
   {
@@ -119,7 +231,7 @@ int main() {
 
     #pragma omp section
     {
-      MyAgent( );
+      ZombieBears( );
     }
   }       
 }
