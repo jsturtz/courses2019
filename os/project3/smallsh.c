cf059d7e170bb42e8a4b466de463927f7b8daaad
jsturtz <jabestur@gmail.com>
2019-05-25 20:21:47 -0700

    Added more work to project3, still incomplete and broken

--- os/project3/smallsh.c
+++ os/project3/smallsh.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #ifndef MAX_ARGS
 #define MAX_ARGS 512
@@ -21,8 +23,8 @@
 #define NUM_PROCESSES 50
 #endif
 
-// precondition: all args are filled with default values indicating empty states
-//               in other words, will only fill values into pointers if discovered
+// global needed for handling CTRL-Z signal
+int   pauseProgram = 0;
 
 void doAtExit() 
 {
@@ -37,11 +39,6 @@ void stripNewline(char* inputBuffer)
   int i = 0; 
   while (inputBuffer[i] != '\n') i++;
   inputBuffer[i] = '\0';
-}
-
-void updateArgs(char** newArgs, char** argsArray) 
-{
-
 }
 
 void deleteArgs(char** args) 
@@ -66,135 +63,46 @@ void addArg(char** args, char* newArg)
   strcpy(args[a], newArg);
 }
 
-// in/out/bg are flags used in main to determine whether to do file redirection or backgrounding
-int setFlags(char** args, int* in, int* out, int* bg) {
-
-}
-
 // splits raw inputBuffer into args string array
-int parse(char* inputBuffer, char** args) {
+int parse(char* inputBuffer, char** args, char* in, char* out, int* bg) {
   
-  // clean up buffers and pointers before parsing inputBuffer
+  // frees all pointers in args, sets to Null
   deleteArgs(args);
 
-  int i = 0;
-  char* token = strtok(inputBuffer, " "); 
-  while (token) 
-  {
-    args[i] = malloc(sizeof(char));
-    strcpy(args[i], token);
-    i++;
-  }
-
-  // THIS IS WHERE I LAST LEFT
-
-  cmd[0] = '\0';
+  // reset in/out/bg
   in[0] = '\0';
   out[0] = '\0';
-  error[0] = '\0';
-  *background = 0;
+  *bg = 0;
 
-  // fill command with first word if exists 
   char* token = strtok(inputBuffer, " "); 
-  if (token) strcpy(cmd, token);
-
-  // loop for args
-  int numArgs = 0;
-  token = strtok(NULL, " ");
-  while (token)
-  {
-    if (token[0] != '<' && token[0] != '>' && token[0] != '&') 
-    {
-      addArg(args, token);
-      numArgs++;
-    }
-    // special case where redirection symbol or background symbol are part of arg 
-    else if (token[1] != '\0') 
-    {
-      addArg(args, token);
-      numArgs++;
-    }
-    // in this case, have reached redirection or background symbols
-    else {
-      break;
-    }
-    token = strtok(NULL, " ");
-  }
-  
-  // loop for redirection / background
-  int inputFound = 0;
-  int outputFound = 0;
-
   while (token) 
   {
-    if (token[0] == '<') 
+    if (token[0] == '<' && token[1] == '\0')
     {
-      // cannot have this symbol twice in string
-      if (inputFound)  
-      {
-        strcpy(error, "Redirection symbol detected twice");
-        return -1;
-      }
-
-      // there must exist another word after that names the file for stdin redirection
-      token = strtok(NULL, " ");
-      if (token) strcpy(in, token);
-      else 
-      {
-        strcpy(error, "Redirection symbol without filename");
-         return -1;
-      }
-
-      inputFound = 1;
+      token = strtok(NULL, " ");        // skip the next word
+      strcpy(in, token);
     }
 
-    else if (token[0] == '>') 
+    else if (token[0] == '>' && token[1] == '\0')
     {
-      // cannot have this symbol twice in string
-      if (outputFound) 
-      {
-        strcpy(error, "Redirection symbol detected twice");
-         return -1;
-      }
-
-      // there must exist another word after that names the file for stdout redirection
-      token = strtok(NULL, " ");
-      if (token) strcpy(out, token);
-      else 
-      {
-        strcpy(error, "Redirection symbol without filename");
-         return -1;
-      }
-        
-      outputFound = 1;
+      token = strtok(NULL, " ");        // skip the next word
+      strcpy(out, token);
     }
 
-    else if (token[0] == '&') 
+    else if (token[0] == '&' && token[1] == '\0')
     {
-      // must be last word of command 
-      token = strtok(NULL, " ");
-      if (token) 
-      {
-        strcpy(error, "Background symbol must be last word");
-         return -1;
-      }
-
-      *background = 1;
+      *bg = 1;
       return 0;
     }
-
-    // only valid symbols after args are redirection or background
     else 
     {
-      strcpy(error, "Invalid argument placement");
-      return -1;
+      addArg(args, token); // allocates space for array, fills with contents of token
     }
-
-    token = strtok(NULL, " ");
+    token = strtok(NULL, " "); 
   }
-  return 0; 
 }
 
+// function that handles built-in "cd" command
 int changeDir(char* path, char* error) 
 {
   if (path) 
@@ -212,41 +120,49 @@ int changeDir(char* path, char* error)
   return 0;
 }
 
-// this is what we want the program to do
-// once handled. A ptr to this function
-// will be passed to the sigaction() func
-void catchSIGINT(int signo) {
-  char* message = "Caught SIGINT\n";
-  write(STDOUT_FILENO, message, 30);
+// function that handles built-in "exit" command
+void exitProgram(int* children, int childrenCount) 
+{
+  int c = 0;
+  for (int c = 0; c < childrenCount; c++) 
+  {
+    kill(children[c], SIGKILL);
+  }
+  exit(0);
 }
 
-void catchSIGTSTP(int signo) {
-  char* message = "Caught SIGINT\n";
-  write(STDOUT_FILENO, message, 30);
+// parent handlers will not terminate shell
+void parentSIGINT(int signo) {
+  int childExitStatus = -5;
+  wait(&childExitStatus);
+  
+  // must display to user which signal killed child process if any
+  if (WIFSIGNALED(childExitStatus))
+  {
+    char message[24];
+    sprintf(message, "terminated by signal %d\n", WTERMSIG(childExitStatus));
+    write(STDOUT_FILENO, message, 24);
+  }
 }
+
+void parentSIGTSTP(int signo) {
+  char message[31];
+  if (pauseProgram) 
+  {
+    strcpy(message, "Exiting foreground-only mode\n");
+    write(STDOUT_FILENO, message, 30);
+  }
+  else 
+  {
+    strcpy(message, "Entering foreground-only mode\n");
+    write(STDOUT_FILENO, message, 31);
+  }
+  pauseProgram = (pauseProgram == 0); // toggle global pause variable
+}
+
+// need this global variable because signal handling requires it
 
 int main() {
-
-  /* // create structs needed for sigaction */
-  /* struct sigaction SIGINT_action = {0}; */
-  /* struct sigaction SIGTSTP_action = {0}; */
-
-  /* // function handlers */
-  /* SIGINT_action.sa_handler = catchSIGINT; */
-  /* SIGTSTP_action.sa_handler = catchSIGTSTP; */
-
-  /* // block all signals while handler active */
-  /* sigfillset(&SIGINT_action.sa_mask); */
-  /* sigfillset(&SIGTSTP_action.sa_mask); */
-
-  /* // no special options */
-  /* SIGINT_action.sa_flags = 0; */
-  /* SIGTSTP_action.sa_flags = 0; */
-  
-  /* // call sigaction to register handlers */
-  /* sigaction(SIGINT, &SIGINT_action, NULL); */
-  /* sigaction(SIGTSTP, &SIGTSTP_action, NULL); */
-
 
   char  inputBuffer[CMD_LENGTH]     = {'\0'};   // holds entire line read in from stdin
   char  cmd[CMD_LENGTH]             = {'\0'};   // holds command to be executed
@@ -254,116 +170,190 @@ int main() {
   char  out[CMD_LENGTH]             = {'\0'};   // holds filename for stdout redirection
   char  startDir[CMD_LENGTH]        = {'\0'};   // holds path for current working directory
   char  error[100]                  = {'\0'};   // holds error message from failed function calls
-  int  children[NUM_PROCESSES]      = { 0 };    // holds error message from failed function calls
+  int   children[NUM_PROCESSES]     = { 0 };    // holds error message from failed function calls
 
-  // args will hold all the pointers to pointers
+  int   childrenCount   = 0;                    // holds number of child processes
+  int   bg              = 0;                    // flag for whether command should be backgrounded
+  int   fgExitStatus    = 0;                    // holds exit status for foregrounded processes that terminate
+  
+  pid_t spawnPid;               
+  int   childExitStatus;  
+
+  struct sigaction SIGINT_action = {0};
+  struct sigaction SIGTSTP_action = {0};
+
+  // signal handlers for parent
+  // function handlers
+  SIGINT_action.sa_handler = parentSIGINT;
+  SIGTSTP_action.sa_handler = parentSIGTSTP;
+
+  // block all signals while handler active
+  sigfillset(&SIGINT_action.sa_mask);
+  sigfillset(&SIGTSTP_action.sa_mask);
+
+  // no special options
+  SIGINT_action.sa_flags = 0;
+  SIGTSTP_action.sa_flags = 0;
+  
+  // call sigaction to register handlers
+  sigaction(SIGINT, &SIGINT_action, NULL);
+  sigaction(SIGTSTP, &SIGTSTP_action, NULL);
+
+  // args will hold all the pointers to pointers, initialized to null
   char** args = (char**) calloc(MAX_ARGS, sizeof(char*));
-  // set all pointers to null (will allocate with addArg and free with deleteArgs)
   for (int i = 0; i < MAX_ARGS; i++) 
   {
     args[i] = NULL;
   }
 
-  FILE* outFile = stdout;           // outFile will point to redirected file
-  FILE* inFile = stdin;
-
-  int background = 0;
-
   while(1) {
 
-    // this is how we should print to screen since printf only writes to buffer. To guarantee that it
-    // writes that buffer to the screen, use fflush to stdout. 
     printf(":");
     fflush(stdout);
+
+    // check if children have become zombies
+    for (int c = 0; c < childrenCount; c++) 
+    {
+      if (waitpid(children[c], &childExitStatus, WNOHANG) != 0)
+      {
+        if (WIFEXITED(childExitStatus))
+        {
+          int exitStatus = WEXITSTATUS(childExitStatus);
+          printf("Process %d completed normally with exit status %d\n", children[c], exitStatus);
+          fflush(stdout);
+        }
+        else
+        {
+          int exitSignal = WIFSIGNALED(childExitStatus);
+          printf("Process %d terminated by signal with exit status %d\n", children[c], exitSignal);
+          fflush(stdout);
+        }
+      }
+    }
 
     // get input from user
     memset(inputBuffer, CMD_LENGTH, '\0');
     fgets(inputBuffer, CMD_LENGTH, stdin);  
     stripNewline(inputBuffer);  
 
-  /* // file redirection */
-  /* int targetFD = open("Somefile.txt", O_WRONLY); */
-  /* // since 1 = stdout, will redirect */
-  /* // from stdout to targetFD */
-  /* int result = dup2(targetFD, 1); */
-
-    if (parse(inputBuffer, cmd, args, in, out, &background, error) != -1) 
+    // fills args with everything needed for execvp
+    // fills in/out with filenames if exist, sets bg flag for parent
+    parse(inputBuffer, args, in, out, &bg); 
+    
+    // do something only if (a) command given or (2) not a comment line
+    if (args[0] && args[0][0] != '\0' && args[0][0] != '#')
     {
-      // only do something if command exists and doesn't begin with comment char
-      if ( cmd[0] != '\0'  && cmd[0] != '#')
+      // built-in commands here
+      if (strcmp(args[0], "exit") == 0) 
       {
-        // built-in commands here
-        if (strcmp(cmd, "exit") == 0) 
+        exitProgram(children, childrenCount);
+      }
+
+      else if (strcmp(args[0], "status") == 0) 
+      {
+        printf("%d\n", fgExitStatus);
+        fflush(stdout);
+      }
+
+      else if (strcmp(args[0], "cd") == 0) 
+      {
+        if (changeDir(args[1], error) == -1) 
         {
-          /* The exit command exits your shell. It takes no arguments. 
-           * When this command is run, your shell must kill any other processes or 
-           * jobs that your shell has started before it terminates itself. */
-          /* exitMyProg(); */
-        }
-
-        else if (strcmp(cmd, "status") == 0) 
-        {
-        
-          /* printStatus(); */
-        }
-
-        else if (strcmp(cmd, "cd") == 0) 
-        {
-          /* Your cd command should support both absolute and relative paths. When smallsh terminates, 
-           * the original shell it was launched from will still be in its original working directory, 
-           * despite your use of chdir() in smallsh. Your shell's working directory begins in whatever 
-           * directory your shell's executible was launched from. */
-          if (changeDir(args[0], error) == -1) 
-          {
-            printf("ERROR: %s\n", error);
-            fflush(stdout);
-          }
-        }
-
-        // do the stuff with exec here
-        else 
-        {
-          pid_t spawnPid = -5;
-          int childExitStatus = -5;
-          spawnPid = fork();
-          switch (spawnPid)
-          {
-            case -1:
-              perror("Hull Breach!\n");
-              exit(1);
-              break;
-            case 0:
-              
-              // set redirection for input
-              if (in[0] != '\0') 
-              {
-                
-              }
-
-              // set redirection for input
-              if (out[0] != '\0') 
-              {
-
-              }
-
-              /* exec(, cmd, args); */
-
-              // the child will have a spawnPID of 0 for itself
-              break;
-            default:
-              // the parent will have a spawnPID of some positive value representing child process
-              break;
-          }
-
-        /* Whenever a non-built in command is received, have the parent fork() off a child. This child then does any needed input/output redirection before running exec() on the command given. Note that when doing redirection, that after using dup2() to set up the redirection, the redirection symbol and redirection destination/source are NOT passed into the following exec command (i.e., if the command given is ls > junk, then you handle the redirection to "junk" with dup2() and then simply pass ls into exec() ). */
+          printf("ERROR: %s\n", error);
+          fflush(stdout);
         }
       }
-    }
 
-    else 
-    {
-      printf("ERROR: %s\n", error);
-      fflush(stdout);
+      // execute non-built in command with new process
+      else 
+      {
+        spawnPid = -5;
+        childExitStatus = -5;
+        spawnPid = fork();
+        switch (spawnPid)
+        {
+          case -1:
+            perror("Hull Breach!\n");
+            exit(1);
+            break;
+
+          case 0:
+
+            // set redirection for output
+            if (out[0] != '\0') 
+            {
+              int outFile = open(out, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+              if (outFile >= 0) 
+              {
+                dup2(outFile, 1);
+                close(outFile);
+              }
+              else 
+              {
+                printf("File could not be opened for writing\n");
+                fflush(stdout);
+                exit(1);
+              }
+            }
+
+            // set redirection for input
+            if (in[0] != '\0') 
+            {
+              int inFile = open(in, O_RDONLY, 0666);
+              if (inFile >= 0) 
+              {
+                dup2(inFile, 0);
+                close(inFile);
+              }
+              else 
+              {
+                printf("File could not be opened for reading\n");
+                fflush(stdout);
+                exit(1);
+              }
+            }
+
+            // in this case, stdin & stdout must be redirected to dev/null
+            if (bg && !pauseProgram) 
+            {
+              int devNull = open("/dev/null", O_RDWR);
+              dup2(devNull, 0);
+              dup2(devNull, 1);
+            }
+
+            if (execvp(args[0], args) == -1)
+            {
+              printf("ERROR: Invalid command\n"); 
+              fflush(stdout);
+              exit(1);
+            }
+            break;
+          default:
+            
+
+            // wait only if backgrounded or paused
+            if (bg && !pauseProgram)
+            {
+              children[childrenCount] = spawnPid;
+              childrenCount++;
+            }
+            else
+            {
+              // since not backgrounded, wait here
+              waitpid(spawnPid, &childExitStatus, 0);
+              
+              if (WIFEXITED(childExitStatus))
+              {
+                fgExitStatus = WEXITSTATUS(childExitStatus);
+              }
+              else if (WIFSIGNALED(childExitStatus))
+              {
+                fgExitStatus = WTERMSIG(childExitStatus);
+              }
+            }
+            break;
+        }
+      }
     }
   }
 
@@ -371,3 +361,6 @@ int main() {
   deleteArgs(args);
   free(args);
 }  
+
+/* Your program must expand any instance of "$$" in a command into the process ID of the shell itself. Your shell does not otherwise perform variable expansion. This feature makes it easier to create a grading script that keeps your work separate. */
+
